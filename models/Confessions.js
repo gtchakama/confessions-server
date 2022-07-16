70090f38206829bc8aa216766d7d875051480380
gtchakama <37905683+gtchakama@users.noreply.github.com>
2022-07-16 05:58:08 +0200

    Update Confessions.js

--- models/Confessions.js
+++ models/Confessions.js
@@ -4,6 +4,7 @@ const ConfessionsSchema = new mongoose.Schema({
   title: { type: String, required: true },
   date: { type: Date, default: Date.now },
   confession: { type: String, required: true },
+  category: { type: String, required: true },
 });
 
 const ConfessionsModel = mongoose.model("confessions", ConfessionsSchema);
