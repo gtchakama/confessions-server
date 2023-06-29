378b021bda67ddb82cce55a736cf0bf37d4b4d70
George T Chakama <37905683+gtchakama@users.noreply.github.com>
2023-06-29 12:34:08 +0200

    Code Clean up :100:

--- index.js
+++ index.js
@@ -1,120 +1,35 @@
+// server.js
+
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const PopularModel = require("./models/popular");
-const ConfessionsModel = require("./models/Confessions");
 const cors = require("cors");
-const path = require("path");
 
 require("dotenv").config();
+
 app.use(express.json());
 app.use(cors({ origin: "*" }));
-// Aanother Cors Fix
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   next();
 });
 
-mongoose.connect(
-  "mongodb://george:chakama@ac-xaiwvv6-shard-00-00.vscsf1s.mongodb.net:27017,ac-xaiwvv6-shard-00-01.vscsf1s.mongodb.net:27017,ac-xaiwvv6-shard-00-02.vscsf1s.mongodb.net:27017/?ssl=true&replicaSet=atlas-ee85j1-shard-0&authSource=admin&retryWrites=true&w=majority"
-);
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    const PORT = process.env.PORT || 3002;
 
-// Getting Request
-app.get("/", (req, res) => {
-  res.set("Access-Control-Allow-Origin", "*");
-  res.sendFile(path.join(__dirname, "./views/index.html"));
-});
+    const confessionRoutes = require("./routes/confessionsRoutes");
 
-// Establishing the port
-const PORT = process.env.PORT || 3002;
-/**
- * /api/createConfession
- * Create a Single Confession
- * Client Side
- */
-app.post("/api/createConfession", async (req, res) => {
-  res.set("Access-Control-Allow-Origin", "*");
-  const confession = req.body;
-  const newConfession = new ConfessionsModel(confession);
-  await newConfession.save();
+app.use("/confessions", confessionRoutes);
 
-  res.json(confession);
-});
-/**
- * /api/SendPopular
- * Create a Single Confession
- * Admin Side
- */
-app.post("/api/SendPopular", async (req, res) => {
-  res.set("Access-Control-Allow-Origin", "*");
-  const confession = req.body;
-  const newConfession = new PopularModel(confession);
-  await newConfession.save();
 
-  if (res.statusCode === 200) {
-    res.json(confession);
-  } else {
-    res.json({ err: "Error creating new Popular Confession." });
-  }
-});
-
-/**
- * /api/deleteConfession/:id
- * Create a Single Confession
- * Admin Side
- */
-
-app.delete("/api/deleteConfession/:id", async (req, res) => {
-  res.set("Access-Control-Allow-Origin", "*");
-  let paramID = req.params.id;
-  const data = await ConfessionsModel.deleteOne({ _id: paramID });
-  res.send(data);
-});
-/**
- * /api/findConfession/:id
- * Get a Single Confession
- *
- */
-app.get("/api/findConfession/:id", async (req, res) => {
-  res.set("Access-Control-Allow-Origin", "*");
-  let paramID = req.params.id;
-  const data = await ConfessionsModel.findOne({ _id: paramID });
-
-  res.json(data);
-});
-
-/**
- * /api/getConfessions
- * Get a Single Confession
- * Client Side
- */
-
-app.get("/api/getConfessions", (req, res) => {
-  res.set("Access-Control-Allow-Origin", "*");
-  ConfessionsModel.find({}, (err, result) => {
-    if (err) {
-      res.json(err);
-    } else {
-      res.json(result);
-    }
+    // Start the server
+    app.listen(PORT, console.log(`Server started on port... ${PORT}`));
+  })
+  .catch((error) => {
+    console.error("Failed to connect to MongoDB:", error);
   });
-});
 
-/**
- * /api/getConfessions
- * Get popular Confessions
- * Client Side
- */
-
-app.get("/api/getpopularConfessions", (req, res) => {
-  res.set("Access-Control-Allow-Origin", "*");
-  PopularModel.find({}, (err, result) => {
-    if (err) {
-      res.json(err);
-    } else {
-      res.json(result);
-    }
-  });
-});
-// Executing the server on given port number
-app.listen(PORT, console.log(`Server started on port ${PORT}`));
